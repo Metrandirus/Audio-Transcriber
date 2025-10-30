17d5a950357a92a77d6f1d6ad561cfa48cc7bc5c
Metrandirus <neurostable@gmail.com>
2025-10-30 19:21:49 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Audio-Transcriber/', // VERY IMPORTANT: This must match your repository name!
-})
